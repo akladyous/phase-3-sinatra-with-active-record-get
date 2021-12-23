fd0c350bb45309771e6d9fa22bff990fff424776
Boula Akladyous <akladyous@gmail.com>
2021-12-23 14:14:22 -0500

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationController < Sinatra::Base
+    # set :default_content_type, "application/json"
 
-  get '/' do
-    { message: "Hello world" }.to_json
-  end
+    get '/' do
+        { message: "Hello world" }.to_json
+    end
 
+    get '/test1/:name' do
+        # params.inspect
+        output = "<div>"
+        output << "<h2>Sinatra First APP</h2>"
+        output << "<p>test params[:variable]</p>"
+        output << "<p>" << "#{params.inspect}" << "</p>"
+        output << "<p>" << "#{params[:namme]}" << "</p>"
+        output << "</div>"
+        output
+    end
+
+    get '/test2/*' do
+        output = "<div>"
+        output << "<h2>Sinatra First APP</h2>"
+        output << "<p>test Splat operator params[:splat]</p>"
+        output << "<p>" << "#{params.inspect}" << "</p>"
+        output << "<p>" << "#{params[:splat]}" << "</p>"
+        output << "</div>"
+        output
+    end
 end
+
+
+        
